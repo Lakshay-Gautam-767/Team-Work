54d04b000c2b125fdd19f1fa2f5ad60bea80da6f
akash <akashnagar97846@gmail.com>
2026-02-18 11:07:58 +0530

    encoding done

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('https://github.com/Lakshay-Gautam-767/Datasets/raw/main/insurance.csv')
+df=pd.read_csv('Team-Work\insurence.csv')
+print(df.head(2))
+
+x=df.drop(columns=['charges'])
+y=df['charges']
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+
+x['sex']=le.fit_transform(x['sex'])
+x['smoker']=le.fit_transform(x['smoker'])
+x['region']=le.fit_transform(x['region'])
+
+print(x.head(2))
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+print(x_train.shape)
+print(x_test.shape)
+
+
