489153e3bc3fc2898e2624536b973a1b3e4dc9ea
Sourabh Sharma <sourabhbagda11@gmail.com>
2026-02-18 12:06:03 +0530

    apply linear regression

--- main.py
+++ main.py
@@ -21,4 +21,14 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42
 print(x_train.shape)
 print(x_test.shape)
 
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(x_train,y_train)
+y_pred =lr.predict(x_test)
+
+from sklearn.metrics import r2_score
+
+print(r2_score(y_test,y_pred))
+
+
 
